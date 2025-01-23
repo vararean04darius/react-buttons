d00e7c265ac969be78c476758650e83f398b8aa4
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2025-01-23 09:32:27 +0200

    added counter with state hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,36 @@ const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [totalCount, addCount] = useState(0);
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    addCount(totalCount + 1);
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? "selected" : ""}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
+    <>
+      <div
+        className="App"
+        style={{
+          backgroundColor,
+        }}
+      >
+        <h1 className="count-container">Counter: {totalCount}</h1>
+        <div className="buttons-container">
+          {COLORS.map((color) => (
+            <button
+              type="button"
+              key={color}
+              onClick={onButtonClick(color)}
+              className={backgroundColor === color ? "selected" : ""}
+            >
+              {color}
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
